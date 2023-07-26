5336d1e3fafde30fc35c3360fe564e5fefe7dc4d
Leeasy <793556890@qq.com>
2023-07-26 13:24:42 +0800

    完善之前的代码

--- transform/SDASCS.py
+++ transform/SDASCS.py
@@ -7,17 +7,19 @@ from audiomentations import SpecCompose, SpecChannelShuffle, SpecFrequencyMask
 
 from SDAfunc import SDA_view, signal, sr
 
-fig = plt.figure(figsize=(10, 6))
-# ,dpi=300
-gs = gridspec.GridSpec(nrows=2, ncols=3, height_ratios=[1, 1])
-
+# 创建梅尔频谱图
 M = librosa.feature.melspectrogram(y=signal, sr=sr, power=1)
 
+# 创建通道混洗增强
 augment = SpecCompose([SpecChannelShuffle(p=1)])
+
+# 将一个通道的梅尔频谱图作为输入，并应用通道混洗增强
 M1 = augment(np.array([M, M]))[0, :, :]
-name1 = "C1"
+name1 = "C1"  # 为增强后的频谱图命名
 
+# 将另一个通道的梅尔频谱图作为输入，并应用通道混洗增强
 M2 = augment(np.array([M, M]))[1, :, :]
-name2 = "C2"
+name2 = "C2"  # 为增强后的频谱图命名
 
-SDA_view()
+# 显示原始频谱图和增强后的频谱图
+SDA_view(M, M1, name1, M2, name2)
